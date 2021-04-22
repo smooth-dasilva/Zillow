cad565f8372bc5fff392218e3d6581eef1f24797
luanf-dasilva <luanf.dasilva28@gmail.com>
2021-04-22 11:06:06 -0400

    Adds proper config test folder usage and organizes

--- Tests/helpersTest.py
+++ Tests/helpersTest.py
@@ -13,6 +13,37 @@ from Experts.FileExpert import FileExpertClass
 
 app_logger = setup_logger('app_logger', './app.log')
 
+
+FileExpert = FileExpertClass(app_logger, r'[A-Za-z0-9]+_time_series.csv',  config.staticTestLocation)
+NoPatterFileExpert = FileExpertClass(app_logger, r'badpattern',  config.dataLocation)
+InvalidDirectoryFileExpert = FileExpertClass(app_logger, r'[A-Za-z0-9]+_time_series.csv', "")
+
+class FileExpertTestCase(unittest.TestCase):
+
+    def test_FileExpertFindsFilesWithPatternWithConstructor(self):
+        self.assertEqual(FileExpert.NameDeq[0]+FileExpert.NameDeq[1], "Another_time_series.csvA_time_series.csv") 
+    def test_FileExpertIgnoresFilesWithoutPatternWithConstructor(self):
+        self.assertEqual(NoPatterFileExpert.NameDeq, Deque([])) 
+
+    def test_FileExpertFindsInputtedDirectory(self):
+        self.assertEqual(FileExpert.CheckPathValidity(), ["Another_time_series.csv", "A_time_series.csv"])
+    def test_FileExpertDoesntFindWrongInputtedDirectory(self):
+        self.assertEqual(InvalidDirectoryFileExpert.CheckPathValidity(), None )
+    
+    
+    def test_FileExpertMatchesRegexPattern(self):
+        self.assertTrue(FileExpert.MatchFilenameToPattern("Metro_time_series.csv"), True)
+    def test_FileExpertDoesntMatchWrongRegexPattern(self):
+        self.assertFalse(FileExpert.MatchFilenameToPattern("Metro_time_series"), False)
+    
+    def test_FileExpertsAddNamesFuncAddsNamesToNameDeqIfMathchesRegex(self):
+        self.assertEqual(FileExpert.AddFileNamesDeq(["New1", "New2", "Metro_time_series.csv"]), Deque(["Metro_time_series.csv"]))
+    
+    def test_FileExpertsReplacesPathToErrorGivenBadPath(self):
+        self.assertEqual(InvalidDirectoryFileExpert.path, "ERROR")
+
+
+
 d = {'Population' : ['37000000','54000000'], 'Record Date' : ['2003-07-15','23-07-1985'], 'RegionName' : ['Texas','California']}
 test_df = pd.DataFrame(data = d)
 convert_df = hlper.convert_col_types(test_df)
@@ -44,33 +75,5 @@ class HelpersTestCase(unittest.TestCase):
         self.assertEqual(convert_df.dtypes['Record Date'], "datetime64[ns]")
         self.assertEqual(convert_df.dtypes['RegionName'], "string")     
 
-
-FileExpert = FileExpertClass(app_logger, r'[A-Za-z0-9]+_time_series.csv',  config.dataLocation)
-NoPatterFileExpert = FileExpertClass(app_logger, r'badpattern',  config.dataLocation)
-InvalidDirectoryFileExpert = FileExpertClass(app_logger, r'[A-Za-z0-9]+_time_series.csv', "")
-
-class FileExpertTestCase(unittest.TestCase):
-
-    def test_FileExpertFindsFilesWithPatternWithConstructor(self):
-        self.assertEqual(FileExpert.NameDeq[0]+FileExpert.NameDeq[1], "Metro_time_series.csvState_time_series.csv") 
-
-    def test_FileExpertFindsInputtedDirectory(self):
-        self.assertEqual(FileExpert.CheckPathValidity(), ["archives","Metro_time_series.csv", "State_time_series.csv"])
-    
-    def test_FileExpertMatchesRegexPattern(self):
-        self.assertTrue(FileExpert.MatchFilenameToPattern("Metro_time_series.csv"), True)
-    
-    def test_FileExpertDoesntMatchWrongRegexPattern(self):
-        self.assertFalse(FileExpert.MatchFilenameToPattern("Metro_time_series"), False)
-    
-    def test_FileExpertsAddNamesFuncAddsNamesToNameDeqIfMathchesRegex(self):
-        self.assertEqual(FileExpert.AddFileNamesDeq(["New1", "New2", "Metro_time_series.csv"]), Deque(["Metro_time_series.csv"]))
-    
-    def test_FileExpertIgnoresFilesWithoutPatternWithConstructor(self):
-        self.assertEqual(NoPatterFileExpert.NameDeq, Deque([])) 
-    
-    def test_FileExpertDoesntFindWrongInputtedDirectory(self):
-        self.assertEqual(InvalidDirectoryFileExpert.CheckPathValidity(), None )
-    
-    def test_FileExpertsReplacesPathToErrorGivenBadPath(self):
-        self.assertEqual(InvalidDirectoryFileExpert.path, "ERROR")
+if __name__ =="__main__":
+    unittest.main()
